c564ca91425d07ff6ab9552561382d0e6c59d0ff
jonki324 <jonki324@gmail.com>
2021-09-29 19:02:29 +0900

    setting vscode java

--- src/test/java/com/example/MemoResourceTest.java
+++ src/test/java/com/example/MemoResourceTest.java
@@ -1,21 +1,16 @@
 package com.example;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import org.junit.jupiter.api.Test;
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class MemoResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+        given().when().get("/hello").then().statusCode(200).body(is("Hello RESTEasy"));
     }
 
 }
